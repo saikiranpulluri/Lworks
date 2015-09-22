589163ba41ba4ebff24aaafc69a6e1ec2d0c220c
Saikiran Pulluri <psk.knr@gmail.com>
2015-09-22 17:57:05 +0530

    Initial commit

---
+++ app/src/main/java/info/androidhive/materialdesign/adapter/SampleTabsAdapter.java
@@ -0,0 +1,38 @@
+package info.androidhive.materialdesign.adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import info.androidhive.materialdesign.activity.PageFragment;
+
+
+/**
+ * Created by saikiran.p on 9/11/2015.
+ */
+public class SampleTabsAdapter  extends FragmentPagerAdapter{
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3" };
+    private Context context;
+    public SampleTabsAdapter(FragmentManager fm,Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
